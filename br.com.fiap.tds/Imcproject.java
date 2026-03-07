e0eb30139b36e13d2d0204f86c41a41cc78b53c1
Alisson Gomes Pereira <alissonu45450@gmail.com>
2026-03-06 21:55:24 -0300

    First exercise
    First exercise in Java

---
+++ br.com.fiap.tds/Imcproject.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+
+public class Imcproject {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // Information about the person
+
+        System.out.print("Name: ");
+        String name = sc.next();
+        System.out.print("Age: ");
+        int age = sc.nextInt();
+        System.out.print("Height: ");
+        double height = sc.nextDouble();
+        System.out.print("Weight: ");
+        int weight = sc.nextInt();
+        System.out.print("Do you practice exercises[Yes/No]: ");
+        String pracExer = sc.next().toUpperCase();
+        int energy = 100;
+
+        // IMC
+
+        double imc = weight / (height * height);
+
+        // Informations Output
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Weight: " + weight);
+        System.out.println(String.format("\nIMC: " + "%.1f", imc));
+
+        // Conditionals age
+
+        if (age >= 18) {
+            System.out.println("\nYou're of legal age.");
+        } else {
+            System.out.println("\nYou're of illegal age.");
+
+        }
+
+        // Conditionals imc
+
+        if (imc > 25 || imc < 18.5) {
+            System.out.println("Your IMC isn't health");
+            energy -= 10;
+        } else { 
+            System.out.println("Your IMC is health");
+            energy += 10;
+        }
+
+        // if practice exercises
+    
+        if (pracExer.startsWith("Y")) {
+            System.out.println("You practice exercises");
+            energy += 5;
+        } else {
+            System.out.println("You don't practice exercises");
+            energy -= 5;
+        }
+            System.out.println("Your energy: " + energy);
+    }
+}
---
+++ br.com.fiap.tds/Teste.java
@@ -0,0 +1,17 @@
+
+public class Teste {
+    public static void main(String[] args) {
+
+        int idade = 68;
+
+        if (idade >= 18) {
+            System.out.println("Maior de Idade");
+            if (idade >= 60) {
+                System.out.println("Terceira");
+            }
+        } else {
+            System.out.println("Menor de idade");
+        }
+    }
+
+}
